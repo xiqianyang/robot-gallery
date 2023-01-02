47bc54eb0d2d461496c07bac7d1c0c39e9e4b4ed
xiqianyang <xiqianyang1@outlook.com>
2023-01-02 10:41:01 +0000

    Yesterday,I learned the hook part of react and tried to practice useState, side effect and useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./assets/images/logo.svg";
 import robots from "./mockdata/robots.json";
 import Robot from "./components/Robot";
@@ -12,56 +12,63 @@ interface State {
   count: number
 }
 
-class App extends React.Component<Props, State> {
-  //*first stage of life cycle: initialization
-  //Initialize component "state"
-  constructor(props) {
-    super(props);
-    this.state = {
-      robotGallery: [],
-      count: 0
+const App: React.FC = (props) => {
+
+  const [count, setCount] = useState<number>(0)
+  const [robotGallery, setRobotGallery] = useState<any>([])
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string>()
+
+  useEffect(() => {
+    document.title = 'Click ${count} time'
+  }, [count]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true)
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users")
+        // .then(response => response.json())
+        // .then(data => setRobotGallery(data))
+        const data = await response.json();
+        setRobotGallery(data);
+      } catch (e) {
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+      }
+      setLoading(false);
     };
-  }
-  //Called after the component has created a dom element and is mounted on the page
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then((data) => this.setState({ robotGallery: data }));
-  }
+    fetchData()
+  }, []);
 
-  //*second stage of life cycle: update
-  //Called when the component receives a new prop (after update)
-  render() {
-    return (
-      <div className={styles.app}>
-        <div className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
-          <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
-        </div>
-        <button onClick={() => {
-          this.setState((preState, preProps) => {
-            return { count: preState.count + 1 }
-          }, () => {
-            console.log("count", this.state.count);
-          });
-          this.setState((preState, preProps) => {
-            return { count: preState.count + 1 }
-          }, () => {
-            console.log("count", this.state.count);
-          });
+  return (
+    <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
+      </div>
+      <button onClick={() => {
+        setCount(count + 1)
 
 
-
-        }}>Click</button>
-        <span>count:{this.state.count}</span>
-        <ShoppingCart />
+      }}>Click</button>
+      <span>count:{count}</span>
+      <ShoppingCart />
+      {!error || error !== ""} && <div>website error: {error}</div>
+      {!loading ? (
         <div className={styles.robotList}>
-          {this.state.robotGallery.map((r) => (
+          {robotGallery.map((r) => (
             <Robot id={r.id} email={r.email} name={r.name} />
           ))}
         </div>
-      </div>
-    );
-  }
+      ) : (
+        <h2>loading</h2>
+      )}
+
+    </div >
+  );
 }
+
 export default App;
